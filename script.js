a23d12abcd97d93fee8336238deafc83910cb27d
victorcaio <146743625+victorcaio@users.noreply.github.com>
2023-10-03 13:16:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function mostra(el){
+    document.getElementById(el).style.display ='block'
+    
+    }
+
+
+
+function esconde(el){
+document.getElementById(el).style.display ='none'
+
+}
+
+   
